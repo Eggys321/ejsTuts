8925e706777e8f2a88fc25b723aad1dae4683aac
eguono imonieroh <imonieroheguono@gmail.com>
2023-11-14 14:57:28 +0100

    fixes

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 5050
+const port = process.env.PORT || 5050;
 const morgan = require("morgan");
-const TASKS = require('./model/taskModel')
+const TASKS = require("./model/taskModel");
 // const mongoose = require("mongoose");
 const connect = require("./db/mongoDB");
 require("dotenv/config");
 
 // custom middlewares
 app.set("view engine", "ejs");
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 // app.use((req,res,next)=>{
 //     console.log('new request made');
 //     console.log('host: ', req.hostname);
@@ -20,48 +20,40 @@ app.use(express.urlencoded({extended:true}));
 
 // TESTING AWA MODEL AND DATABASE
 // .save() mthod is a mongoose method for saving data to awa database
-app.get('/post-task',async(req,res)=>{
-    const testData = new TASKS({
-        name:'Halimat',
-        title:'front-end',
-        task:'she sabi teach front-end die!!!'
-    })
+app.get("/post-task", async (req, res) => {
+  const testData = new TASKS({
+    name: "Chimelu",
+    title: "back-end",
+    task: "missed class yesterday!",
+  });
 
-    try{
-        const newTask = await testData.save();
-        res.status(201).send(newTask)
-
-    }catch(error){
-        console.log(error);
-    }
-
-
-})
+  try {
+    const newTask = await testData.save();
+    res.status(201).send(newTask);
+  } catch (error) {
+    console.log(error);
+  }
+});
 // .find() method is a mongoose method for getting all d data from awa database
 
-app.get('/get-posts',async(req,res)=>{
-    try{
-        const getTasks =await TASKS.find();
-        res.status(200).send(getTasks)
-
-
-    }catch(error){
-        console.log(error);
-    }
-})
+app.get("/get-posts", async (req, res) => {
+  try {
+    const getTasks = await TASKS.find();
+    res.status(200).send(getTasks);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 // .findById() method is a mongoose method for finding a specific data from awa database
-app.get('/single-task',async(req,res)=>{
-    try{
-        const singleTask = await TASKS.findById('65523309d4b4472d5606a998');
-        res.status(200).send(singleTask)
-        
-    }catch(error){
-        console.log(error);
-    }
-
-})
-
+app.get("/single-task", async (req, res) => {
+  try {
+    const singleTask = await TASKS.findById("655345429e5e6e9df3fe1203");
+    res.status(200).send(singleTask);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 // END OF DATABASE TEST
 
@@ -87,37 +79,44 @@ app.use(express.static("public"));
 // ];
 
 // api
-app.post('/api/v1/create',async(req,res)=>{
-    const ss = new TASKS(req.body)
-
-//   ss.save()
-//   .then((result)=>{
-//     res.redirect('/')
-
-//   })
-try{
-    await ss.save();
-    res.status(200).redirect('/')
-
-}catch(error){
+// post route
+app.post("/api/v1/create", async (req, res) => {
+  // console.log(req.body);
+  const newTask = new TASKS(req.body);
+  try {
+    await newTask.save();
+    res.status(201).redirect("/");
+  } catch (error) {
     console.log(error);
-}
+  }
+});
+// route params 
+app.get('/api/v1/route/:id',async(req,res)=>{
+  const id = req.params.id
+  console.log(id);
+  try{
+    const result = await TASKS.findById(id)
+    res.status(200).render('singlepage' , {title:'single ||page',task:result})
+
+  }catch(error){
+    console.log(error);
+  }
+
 
 })
 // page routes
-app.get("/", async(req, res) => {
-    // TASKS.find()
-    // .then((result)=>{
+app.get("/", async (req, res) => {
+  // TASKS.find()
+  // .then((result)=>{
 
-    //     res.render("index", { title: "Home || Page", tasks:result });
-    // })
-    try{
-        const result = await TASKS.find()
-        res.render("index", { title: "Home || Page", tasks:result });
-
-    }catch(error){
-        console.log(error);
-    }
+  //     res.render("index", { title: "Home || Page", tasks:result });
+  // })
+  try {
+    const result = await TASKS.find();
+    res.render("index", { title: "Home || Page", tasks: result });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 app.get("/about", (req, res) => {
